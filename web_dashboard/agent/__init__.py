59928e971b956b5ef206c2aec55286ff0487e118
ravik789 <ravikalagara3018@gmail.com>
2026-06-03 14:08:01 -0500

    Initialize QA Job Automation Agent package

---
+++ web_dashboard/agent/__init__.py
@@ -0,0 +1,11 @@
+"""
+QA Job Automation Agent Package
+Automates job searching and application process for QA positions
+"""
+
+__version__ = "1.0.0"
+__author__ = "QA Job Agent"
+
+from .application_tracker import ApplicationTracker
+from .resume_parser import ResumeParser
+from .job_matcher import JobMatcher
